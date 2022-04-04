071a6bdf3d32d29bb378b04d96e0ce6419ba2d7a
mgecelewicz <mgecelewicz@gmail.com>
2022-04-04 16:39:53 -0300

    subi la linea de app.use como lo muestra el video antes del server

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const path = require("path");
 
 const app = express();
 const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("servidor funcionando en puerto 3000"));
-app.use(express.static(publicPath));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"));
