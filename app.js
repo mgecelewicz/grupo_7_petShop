fe14854f4634ca8a245a5c29a91f621c40afedb7
Matias Molina <molina.matias.87@gmail.com>
2022-04-13 02:04:14 -0300

    link iconos//link css//inicio header

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("servidor funcionando en puerto 3000"));
 
-
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
