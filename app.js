790e04e741f2105953781b4a4782ce3f2c2a514d
mgecelewicz <mgecelewicz@gmail.com>
2022-05-02 21:41:10 -0300

    update

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const publicPath = path.resolve(dirname, "./public");
+const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("servidor funcionando en puerto 3000"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(dirname, "./views/index.html"));
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(dirname, "./views/register.html"));
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
 app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(dirname, "./views/login.html"));
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
 
 app.get("/producto", (req, res) => {
